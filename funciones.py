a15c7820f326fabda8105a9efc918a369a37d976
Dayana Quintero Franco <daquifra.15@gmail.com>
2022-08-17 14:52:41 -0500

    creacion de funciones suma y multiplicacion y cambio de nombre

---
+++ funciones.py
@@ -0,0 +1,15 @@
+def suma(num1: int,num2: int) ->int:
+    resultado = num1 + num2
+    return resultado
+
+resp = suma('Santiago',' Echeverri')
+print(resp)
+
+
+
+def multiplicacion(num1,num2):
+    resultado = num1*num2
+    return resultado 
+
+resp = multiplicacion(8,10)
+print(resp)
